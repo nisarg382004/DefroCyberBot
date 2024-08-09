4db7d638cb5bd0b652a3281abcc032e43f091595
Nisarg Kumawat <141359722+nisarg382004@users.noreply.github.com>
2024-08-09 16:06:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Activate the virtual environment
 echo "Activating the virtual environment..."
 source venv/bin/activate
 
-# Run the bot
 echo "Starting DefroCyberBot..."
 python src/bot.py
