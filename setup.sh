8b96cc45ba88279cd1db2f0c8fc6e735154695ee
Nisarg Kumawat <141359722+nisarg382004@users.noreply.github.com>
2024-08-14 10:35:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Activating the virtual environment..."
+# Activate the virtual environment
 source venv/bin/activate
 
-echo "Starting DefroCyberBot..."
-python3 bot.py
+# Install additional Python dependencies
+pip install schedule SpeechRecognition aiohttp requests beautifulsoup4 spacy
+
+# Download and install the spaCy model
+python -m spacy download en_core_web_sm
+
+echo "Setup complete. You can now run 'python3 cli.py' to start the bot."
